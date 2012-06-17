db0e22738d8735ca818ecc315d681c4783daca8b
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2012-06-17 13:47:04 +0200

    Add very rudimentary parser.
    Currently only handles simple expressions of multiplication.

--- scheme.py
+++ scheme.py
@@ -8,3 +8,41 @@ class Tokenizer(object):
         tokens = string.split()
         return tokens
 
+class Symbol(object):
+    def __init__(self, token):
+        self.token = token
+
+class Operator(object):
+    pass
+
+class Number(object):
+    def __init__(self, token):
+        self.value = token
+
+class Parser(object):
+    @staticmethod 
+    def parse(tokens):
+        tree = []
+        for t in tokens:
+            if t != "(" and t != ")":
+                tree.append(Parser.make_expression(t))
+        return tree
+
+    @staticmethod
+    def make_expression(token):
+        if token == "*":
+            return Operator()
+        elif Parser.is_number(token):
+            return Number(token)
+        else:
+            return Symbol(token)
+        
+        
+    @staticmethod
+    def is_number(token):
+        try:
+            float(token)
+            return True
+        except:
+            return False
+            
--- scheme_test.py
+++ scheme_test.py
@@ -2,7 +2,9 @@
 # -*- coding: latin-1 -*-
 
 import unittest
-from scheme import Tokenizer
+from scheme import Tokenizer, Parser
+
+from scheme import Operator, Symbol, Number
 
 
 class TestTokenizer(unittest.TestCase):
@@ -16,6 +18,21 @@ class TestTokenizer(unittest.TestCase):
         self.assertEqual(len(tokens), 10)
         self.assertEqual(tokens, ["(", "+", "(", "a", ")", "(", "b", ")", "c", ")"])
 
+class TestParser(unittest.TestCase):
+    def test_can_parse_simple_tokens(self):
+        tokens = ["(", "*", "a", "b", ")"]
+        parse_tree = Parser.parse(tokens)
+        self.assertEqual(type(parse_tree), list)
+        self.assertEqual(len(parse_tree), 3)
+        self.assertEqual(type(parse_tree[0]), Operator) 
+        self.assertEqual(type(parse_tree[1]), Symbol) 
+        self.assertEqual(type(parse_tree[2]), Symbol) 
+        
+    def test_can_parse_floating_point_numbers(self):
+        tokens = ["(", "3.14", ")"]
+        parse_tree = Parser.parse(tokens)
+        self.assertEqual(len(parse_tree), 1)
+        self.assertEqual(type(parse_tree[0]), Number) 
 
 if __name__ == '__main__':
     unittest.main()
